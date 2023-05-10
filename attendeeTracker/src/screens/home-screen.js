23bea4f0b4edb24816dc0f42ba2d79f9bc338fc4
Julian Baldwin <julianbaldwin97@gmail.com>
2023-05-10 09:04:50 -0500

    made filtering based on location and modality

--- attendeeTracker/src/screens/home-screen.js
+++ attendeeTracker/src/screens/home-screen.js
@@ -4,60 +4,6 @@ import './styles.css';
 import Header from '../components/header';
 import { useState } from 'react';
 
-// const ProfileRow = () => {
-//   const row = [];
-//   for (var i = 0; i < 6; i++)
-//     row.push(
-//       <div key={i}>
-//         <div className='user'></div>
-//         <h3 className="nameText">Name</h3>
-//         <p className="positionText">Position</p>
-//       </div>
-//     );
-//   return row;
-// }
-//
-// const ProfileGrid = () => {
-//   const row = []
-//     for (var i = 0; i < 4; i++)
-//       row.push(
-//         <div key={i} className='userRow'>
-//           <ProfileRow />
-//         </div>
-//       );
-//   return row;
-// }
-
-// const ProfileCol = (user) => {
-//   const row = [];
-//   for (var i = 0; i < 6; i++)
-//     console.log('user', user.user.name)
-//     row.push(
-//         <div key={i}>
-//           <div className='user'></div>
-//           <h3 className="nameText">{user.user.name}</h3>
-//           <p className="positionText">{user.user.role}</p>
-//         </div>
-//     );
-//   return row;
-// }
-
-const ProfileGrid = (props) => {
-  const grid = [];
-
-  props.profiles.forEach((profile, i) => {
-    grid.push(
-      <div key={i} className='userCard'>
-        {/* <div className='user'></div> */}
-        <img src={'https://picsum.photos/100?random='+ String(i)} alt="profilePic" className='profilePic'></img>
-        <h3 className="nameText">{profile.name}</h3>
-        <p className="positionText">{profile.role}</p>
-      </div>
-    );
-  });
-
-  return grid;
-}
 
 const format_profiles = raw => {
   const result = [];
@@ -70,7 +16,41 @@ const format_profiles = raw => {
 
 const HomeScreen = (props) => {
   const [profiles] = useState(format_profiles(props.raw_people));
-  // console.log(profiles);
+  // state for dropdown menus
+  const [locationSelect, setLocationSelect] = useState("Porto Alegre");
+  const handleLocationChange = (event) => {
+    setLocationSelect(event.target.value);
+  }
+  const [modalitySelect, setModalitySelect] = useState("On-Site");
+  const handleModalityChange = (event) => {
+    setModalitySelect(event.target.value);
+  }
+
+  const ProfileGrid = () => {
+    const grid = [];
+
+    const filtered = profiles.filter(p => {
+      const loc = p.office === locationSelect;
+      const mode = p.status === modalitySelect.toLowerCase();
+
+      return loc && mode;
+    });
+  
+    filtered.forEach((profile, i) => {
+      // console.log(profile.status, modalitySelect);
+
+      grid.push(
+        <div key={i} className='userCard'>
+          {/* <div className='user'></div> */}
+          <img src={'https://picsum.photos/100?random='+ String(parseInt(profile.id))} alt="profilePic" className='profilePic'></img>
+          <h3 className="nameText">{profile.name}</h3>
+          <p className="positionText">{profile.role}</p>
+        </div>
+      );
+    });
+  
+    return grid;
+  }
 
   return (
     <div>
@@ -81,7 +61,8 @@ const HomeScreen = (props) => {
         <h2 className='whereIsEveryoneText'>Where is everyone?</h2>
         <div className='select'> 
           <div className='location'>
-            <select className='locationDropdown' name="location" id="location">
+            <select className='locationDropdown' name="location" id="location" 
+              value={locationSelect} onChange={handleLocationChange}>
               <option value="Select location" disabled>Select location</option>
               <option value="Porto Alegre">Porto Alegre</option>
               <option value="Sao Paulo">Sao Paulo</option>
@@ -90,7 +71,8 @@ const HomeScreen = (props) => {
             </select>
           </div>
           <div className='modality'>
-            <select className='modalityDropdown' name="modality" id="modality">
+            <select className='modalityDropdown' name="modality" id="modality"
+              value={modalitySelect} onChange={handleModalityChange}>
               <option value="Select modality" disabled>Select modality</option>
               <option value="On-Site">On-Site</option>
               <option value="Remote">Remote</option>
