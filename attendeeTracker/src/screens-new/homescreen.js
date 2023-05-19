b1084bf975f300b4c5e4bf65530e9102664349de
jesusm2024 <jesusmontero2024@u.northwestern.edu>
2023-05-18 20:40:44 -0500

    Name search with department select (still working).

--- attendeeTracker/src/screens-new/homescreen.js
+++ attendeeTracker/src/screens-new/homescreen.js
@@ -16,6 +16,7 @@ const format_profiles = raw => {
   }
 
 const Homescreen = (props) => {
+
     const [profiles] = useState(format_profiles(props.raw_people));
     // state for dropdown menus
     const [locationSelect, setLocationSelect] = useState("Select location");
@@ -26,9 +27,10 @@ const Homescreen = (props) => {
     const handleModalityChange = (event) => {
       setModalitySelect(event.target.value);
     }
-  
+
     const ProfileGrid = () => {
       const grid = [];
+      const employees = [];
   
       const filtered = profiles.filter(p => {
         const loc = p.office === locationSelect || locationSelect === "Select location";
@@ -39,6 +41,8 @@ const Homescreen = (props) => {
     
       filtered.forEach((profile, i) => {
         // console.log(profile.status, modalitySelect);
+
+        employees.push(profile);
   
         grid.push(
           <div key={i} className='userCard'>
@@ -50,7 +54,117 @@ const Homescreen = (props) => {
         );
       });
     
-      return grid;
+      return {grid, employees};
+    }
+
+    // Set initial grid to all employees.
+    const [grid, setGrid] = useState(ProfileGrid().grid);
+
+    // Actual profile data.
+    const [employees, setEmployees] = useState(ProfileGrid().employees);
+
+    // Whether a at least one department has been selected.
+    const [departmentSelected, setDepartmentSelected] = useState(false);
+
+    const [searchBoxEmpty, setSearchBoxEmpty] = useState(true);
+
+    const NameSearch = () => {
+
+      const grid = [];
+      const newEmployees = [];
+      
+      // Retrieve search box element.
+      let name = document.getElementById('search');
+
+      // Search box is empty. 
+      if (name === null){
+        setSearchBoxEmpty(true);
+        setGrid(ProfileGrid().grid);
+        setEmployees(ProfileGrid().employees);
+        return;
+      }
+      // Search box is not empty (lowercase & remove spaces). 
+      name = name.value.toLowerCase().split(" ").join("");
+
+      // Iterate through profiles to find matching name. 
+      profiles.forEach((profile, i) => {
+        if (profile.name.toLowerCase().split(" ").join("").substring(0,name.length) === name) {
+          newEmployees.push(profile);
+          grid.push(
+            <div key={i} className='userCard'>
+              {/* <div className='user'></div> */}
+              <img src={'https://picsum.photos/100?random='+ String(parseInt(profile.id))} alt="profilePic" className='profilePic'></img>
+              <h3 className="nameText">{profile.name}</h3>
+              <p className="positionText">{profile.role}</p>
+            </div>
+          );
+        }
+      })
+
+      // Update the grid and employees.
+      setSearchBoxEmpty(false); 
+      setGrid(grid);
+      console.log('newEmployees', newEmployees);
+      setEmployees(newEmployees);
+
+      // Update the grid if at least one department is selected.
+      // if (departmentSelected){
+      //     DepartmentSelect();
+      // }
+    }
+
+    const DepartmentSelect = () => {
+      
+      // Store whether a department has been selected.
+      const marketing = document.getElementById('marketing').checked;
+      const design = document.getElementById('design').checked;
+      const softwareEngineering = document.getElementById('softwareEngineering').checked;
+      const staff = document.getElementById('staff').checked;
+      const dataScience = document.getElementById('dataScience').checked;
+      const mobile = document.getElementById('mobile').checked;
+
+      // Store all checklist bools. 
+      const departments = [marketing, design, softwareEngineering, staff, dataScience, mobile];
+
+      // Reload all profiles if no departments are selected.
+      if (departments.every(d =>d === false)){
+        setDepartmentSelected(false);
+        setGrid(ProfileGrid().grid);
+        setEmployees(ProfileGrid().employees);
+        return;
+      }
+
+      // At least one department selected and search box empty. 
+
+      // Strings to compare department. 
+      const depString = ['marketing', 'design', 'softwareEngineering','staff', 'dataScience', 'mobile'];
+
+      const grid = [];
+
+      // Iterate through the departments.
+      departments.forEach((department, j) => {
+        // Department is selected. 
+        if (department){
+          // Iterate through the current list of employee. 
+          profiles.forEach((profile, i) => {
+            // Check if the profile is part of the current department. 
+            if (profile.department === depString[j]){
+              grid.push(
+                <div key={i} className='userCard'>
+                  {/* <div className='user'></div> */}
+                  <img src={'https://picsum.photos/100?random='+ String(parseInt(profile.id))} alt="profilePic" className='profilePic'></img>
+                  <h3 className="nameText">{profile.name}</h3>
+                  <p className="positionText">{profile.role}</p>
+              </div>
+              );
+            } 
+          });
+        }
+      });
+     
+      // Update the grid.
+      setDepartmentSelected(true);
+      setGrid(grid);
     }
   
     return (
@@ -79,27 +193,37 @@ const Homescreen = (props) => {
                 </div>
                 <div className='whosInHeading'>
                   <h2 className='whosIn'>Who's in the office today</h2>
-                  <p>| 45 people</p>
+                  <p>| {grid.length} people</p>
                 </div>
                 <div className='headerFilters'>
                     <form className='searchEmployee'>
                       <FontAwesomeIcon icon={faMagnifyingGlass} />
-                      <input placeholder='Looking for someone?' type='search'></input>
+                      <input placeholder='Looking for someone?' type='search' id='search' onChange={NameSearch}></input>
                     </form>
                     {/* these should probably be checkboxes eventually */}
-                    <div className='filterOptions'>
-                        <div id='marketing'>Marketing</div>
+                    {/* <div className='filterOptions'>
+                        <div id='marketing' onClick={DepartmentSelect}>Marketing</div>
                         <div id='design'>Design</div>
                         <div id='softwareEngineering'>Software Engineering</div>
                         <div id='staff'>Staff</div>
                         <div id='dataScience'>Data Science</div>
                         <div id='mobile'>Mobile</div>
-                    </div>
+                    </div> */}
+                    
+                    <form onChange={DepartmentSelect}>
+                      <label><input type='checkbox' name='marketing' value='marketing' id='marketing'/>Marketing</label>
+                      <label><input type='checkbox' name='design' value='design' id='design'/>Design</label>
+                      <label><input type='checkbox' name='softwareEngineering' value='softwareEngineering' id='softwareEngineering'/>Software Engineering</label>
+                      <label><input type='checkbox' name='staff' value='staff' id='staff'/>Staff</label>
+                      <label><input type='checkbox' name='dataScience' value='dataScience' id='dataScience'/>Data Science</label>
+                      <label><input type='checkbox' name='mobile' value='mobile' id='mobile'/>Mobile</label>
+                    </form>
                 </div>
             </div>
             {/* Grid */}
             <div className='userDisplay'> 
-                <ProfileGrid profiles={profiles} />
+                {/* <ProfileGrid profiles={profiles} /> */}
+                {grid.length > 0 ? grid : <p>No employee/s found</p>}
             </div>
         </div>
       </div>
