148ac95847958adaf4520ca255863218a4837d83
George Bian <bianz20@berkeley.edu>
2023-05-22 22:30:58 -0500

    Passed dummy test

--- attendeeTracker/src/App.test.js
+++ attendeeTracker/src/App.test.js
@@ -1,8 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+it('has different department', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  await screen.findByText(/department/);
 });
