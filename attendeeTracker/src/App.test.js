c0d0e9b739039daefd49c1553aa8527c05ac54eb
Julian Baldwin <julianbaldwin97@gmail.com>
2023-05-22 22:52:37 -0500

    attempting mock network test cases

--- attendeeTracker/src/App.test.js
+++ attendeeTracker/src/App.test.js
@@ -1,7 +1,33 @@
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
+
+import { useDbData } from './utilities/firebase';
+
+// vi.mock('../utilities/firebase');
+
+const mockProfiles = {
+  "88888": {
+    "department": "marketing",
+    "name": "Abby",
+    "office": "Lisbon",
+    "role": "Manager",
+    "status": "remote"
+  }
+};
 
 it('has different department', async () => {
   render(<App />);
   await screen.findByText(/department/);
+});
+
+it('mocking network call', () => {
+  // useData.mockReturnValue([mockSchedule, false, null]);
+  // useUserState.mockReturnValue(null);
+
+  // useDbData.mockReturnValue(mockProfiles);
+
+  render(<App />);
+  const name = screen.queryByText(/Abby/);
+  expect(name).toBeInTheDocument();
 });
